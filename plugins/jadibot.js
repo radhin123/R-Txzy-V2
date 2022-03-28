be1a49ed1e5e73e8805ab01a877bb9f5ad8948bd
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-03-28 09:40:31 +0700

    Y

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       auth = true
     }
     conn.on('qr', async qr => {
-      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk Perangkat Tertaut\n3. Scan QR ini \nQR Expired dalam 20 detik\n4. Jangan lupa Donasi Dana 082221792667', m)
+      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk Perangkat Tertaut\n3. Scan QR ini \nQR Expired dalam 20 detik\n4. ', m)
       setTimeout(() => {
         parent.deleteMessage(m.chat, scan.key)
       }, 30000)
@@ -72,8 +72,8 @@ handler.help = ['jadibot']
 handler.tags = ['jadibot', 'premium']
 
 handler.command = /^jadibot$/i
-handler.premium = true
-handler.private = false
+handler.premium = false
+handler.private = true
 handler.limit = 1
 
 module.exports = handler
