2dc31684301ed0337330025604e9eb4ed96b14f4
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-04-06 20:35:24 +0700

    update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -4,7 +4,7 @@ let handler = async (m, { usedPrefix, conn, command, args }) => {
   if (!args[0]) throw `Harap masukkan URL yang ingin di screenshot!\n\nContoh: ${usedPrefix + command} https://nekopoi.care`
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-  conn.sendFile(m.chat, ss, 'screenshot.png', url, m, 0, { thumbnail: ss })
+  conn.sendMessage(m.chat, ss,from, buff, image, url, m, 0, { thumbnail: ss })
 }
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
